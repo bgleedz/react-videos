5014b558cc30fc40db04703312ee14873e4220b1
Brandee Gleed <brandeegleed1@gmail.com>
2020-09-30 10:24:54 -0400

    react videos search

---
+++ src/apis/youtube.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+
+const KEY = 'AIzaSyBVMYBTmBtrLxWGkQWY-aMun-k2kyn2etY';
+
+
+export default axios.create ({
+	baseURL: 'https://www.googleapis.com/youtube/v3',
+	params: {
+		part: 'snippet',
+		maxResults: 5,
+		key: KEY
+
+	}
+});
